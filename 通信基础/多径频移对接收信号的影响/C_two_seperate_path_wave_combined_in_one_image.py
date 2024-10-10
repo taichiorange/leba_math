0757a2935a34286ffd8f93661a479e37d2e453a8
leba <lawson.huang23@gmail.com>
2024-10-09 22:14:23 -0400

    add

---
+++ 通信基础/多径频移对接收信号的影响/C_two_seperate_path_wave_combined_in_one_image.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct  9 22:10:09 2024
+
+@author: 41377
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+PI = np.pi
+
+t = np.arange(0,0.3,0.0001)
+
+fc = 100
+
+fd1 = 5
+fd2 = 7
+
+s1 = np.cos(2*PI*(fc-fd1)*t)
+s2 = np.cos(2*PI*(fc-fd2)*t)
+
+r = s1 + s2
+
+fig,ax1 = plt.subplots(1,1)
+ax1.plot(t,r)

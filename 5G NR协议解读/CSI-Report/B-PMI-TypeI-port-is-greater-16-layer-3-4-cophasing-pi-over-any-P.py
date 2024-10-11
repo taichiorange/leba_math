9e1342f8abcfa26155c90f15ee598bcd4b27ec1a
leba <lawson.huang23@gmail.com>
2024-10-10 20:06:38 -0400

    add

---
+++ 5G NR协议解读/CSI-Report/B-PMI-TypeI-port-is-greater-16-layer-3-4-cophasing-pi-over-any-P.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 24 00:39:21 2023
+
+@author: 41377
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+N = 8
+O1 = 4
+dVsLambda = 0.5
+k = 2
+n = np.arange(0,N,1)
+nHalf = np.arange(0,N/2,1)
+fig = plt.figure(figsize=(15,15))
+
+P = 16
+
+########################################
+#  把 beam vector 分成上下两部分，第二部分在第一部分的基础上乘以一个相位偏转
+#  PI/4 * p , p = 0,1,2,3
+########################################
+legendStr = []
+for p in range(0,P):
+    phaseRotateVectorFirstHalf = np.exp(1j*4*k*np.pi/(N*O1)*nHalf)
+    phaseRotateVectorSecondHalf = phaseRotateVectorFirstHalf * np.exp(1j*p*np.pi/P)
+    phaseRotateVector =np.concatenate((phaseRotateVectorFirstHalf,phaseRotateVectorSecondHalf), axis=0) 
+    #print(phaseRotateVector)
+    thetaAll = np.arange(0,2*np.pi,0.01)
+    beamAmp = []
+    for theta in thetaAll:
+        ePhi = np.exp(1j*2*np.pi*dVsLambda*np.cos(theta)*n)
+        BeamAmpForOneTheta = np.dot(phaseRotateVector,ePhi)
+        beamAmp.append(BeamAmpForOneTheta)
+    beamAmp = np.array(beamAmp)/N
+
+    legendStr.append(str(p)+"/" + str(P) + "pi")
+    
+    ax = plt.subplot(111, polar=True)
+    ax.plot(thetaAll, abs(beamAmp),"--")
+    #ax.vlines(beamAngle,0,16,'r--')
+    ax.grid(True)
+    ax.set_theta_offset(-1*np.pi/2)
+    
+########################################
+#  把 beam vector 分成上下两部分，只画上半部分的
+########################################
+phaseRotateVector2 = np.exp(1j*4*k*np.pi/(N*O1)*nHalf)
+thetaAll = np.arange(0,2*np.pi,0.01)
+#print(phaseRotateVector2)
+beamAmp = []
+for theta in thetaAll:
+    ePhi = np.exp(1j*2*np.pi*dVsLambda*np.cos(theta)*nHalf)
+    BeamAmpForOneTheta = np.dot(phaseRotateVector2,ePhi)
+    beamAmp.append(BeamAmpForOneTheta)
+beamAmp = np.array(beamAmp)/(N/2)
+ax = plt.subplot(111, polar=True)
+ax.plot(thetaAll, abs(beamAmp),"-")
+ax.grid(True)
+ax.set_theta_offset(-1*np.pi/2)
+    
+########################################
+#  把 beam 用完整的，但是相位旋转的依然是 k
+########################################
+phaseRotateVector2 = np.exp(1j*4*k*np.pi/(N*O1)*n)
+thetaAll = np.arange(0,2*np.pi,0.01)
+beamAmp = []
+for theta in thetaAll:
+    ePhi = np.exp(1j*2*np.pi*dVsLambda*np.cos(theta)*n)
+    BeamAmpForOneTheta = np.dot(phaseRotateVector2,ePhi)
+    beamAmp.append(BeamAmpForOneTheta)
+beamAmp = np.array(beamAmp)/N
+ax = plt.subplot(111, polar=True)
+ax.plot(thetaAll, abs(beamAmp),"r.")
+ax.grid(True)
+ax.set_theta_offset(-1*np.pi/2)
+
+legendStr.append("N/2 normal");
+legendStr.append("N normal");
+ax.legend(legendStr)  
